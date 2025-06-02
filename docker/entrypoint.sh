b9b4d0bf8b2cb8d2c347f95b3843f03cc16b4b77
Vezpi <etienne.girault@gmail.com>
2025-06-02 08:02:23 +0000

    add: docker folder for image build

---
+++ docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# Configuration
+REPO_URL="${REPO_URL:-https://git.vezpi.me/Vezpi/blog.git}"
+BRANCH="${BRANCH:-preview}"
+CLONE_DIR="${CLONE_DIR:-/blog}"
+
+echo "Cloning $REPO_URL (branch: $BRANCH)..."
+git clone --depth 1 --branch "$BRANCH" "$REPO_URL" "$CLONE_DIR"
+
+echo "Building site with Hugo $HUGO_VERSION..."
+hugo --source "$CLONE_DIR" --destination "$HUGO_DEST" --cleanDestinationDir
+
+echo "Starting Nginx..."
+exec nginx -g 'daemon off;'
