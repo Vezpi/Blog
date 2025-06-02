687595b4c9dabedfea4130a6986fdd42cc53add0
Vezpi <etienne.girault@gmail.com>
2025-06-02 09:21:09 +0000

    fix: add recurse-submodules in the git clone

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,7 @@ BRANCH="${BRANCH:-preview}"
 CLONE_DIR="${CLONE_DIR:-/blog}"
 
 echo "Cloning $REPO_URL (branch: $BRANCH)..."
-git clone --depth 1 --branch "$BRANCH" "$REPO_URL" "$CLONE_DIR"
+git clone --depth 1 --recurse-submodules --branch "$BRANCH" "$REPO_URL" "$CLONE_DIR"
 
 echo "Building site with Hugo $HUGO_VERSION..."
 hugo --source "$CLONE_DIR" --destination "$HUGO_DEST" --cleanDestinationDir
