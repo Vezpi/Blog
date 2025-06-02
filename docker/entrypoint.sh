e835486965accc091352b3e138bbed9a102108bf
Vezpi <etienne.girault@gmail.com>
2025-06-02 13:04:51 +0000

    change: add HUGO_DEST and remove metrics

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,12 +6,11 @@ REPO_URL="${REPO_URL:-https://git.vezpi.me/Vezpi/blog.git}"
 BRANCH="${BRANCH:-preview}"
 CLONE_DIR="${CLONE_DIR:-/blog}"
 
-echo "Cloning $REPO_URL (branch: $BRANCH)..."
+echo "- Cloning $REPO_URL (branch: $BRANCH)..."
 git clone --depth 1 --recurse-submodules --branch "$BRANCH" "$REPO_URL" "$CLONE_DIR"
 
-echo "Building site with Hugo $HUGO_VERSION..."
-hugo --source "$CLONE_DIR" --destination "$HUGO_DEST" --logLevel info --cleanDestinationDir --gc --panicOnWarning --printI18nWarnings --templateMetrics --templateMetricsHints
+echo "- Building site with Hugo v$HUGO_VERSION in $HUGO_DEST..."
+hugo --source "$CLONE_DIR" --destination "$HUGO_DEST" --logLevel info --cleanDestinationDir --gc --panicOnWarning --printI18nWarnings
 
-
-echo "Starting Nginx..."
+echo "- Starting Nginx..."
 exec nginx -g 'daemon off;'
