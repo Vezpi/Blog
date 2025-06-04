eab0acaec0828bccdf0fc962569711c5ae0dc5a1
Vezpi <etienne.girault@gmail.com>
2025-06-04 21:18:48 +0000

    add: URL and drafts

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,14 +3,21 @@ set -e
 
 # Configuration
 REPO_URL="${REPO_URL:-https://git.vezpi.me/Vezpi/blog.git}"
+URL="${URL:-blog.vezpi.com}"
 BRANCH="${BRANCH:-preview}"
 CLONE_DIR="${CLONE_DIR:-/blog}"
+DRAFTS=""
+
+# Add drafts for preview
+if [ $BRANCH -eq "preview" ]; then
+  DRAFTS="--buildDrafts"
+fi
 
 echo "- Cloning $REPO_URL (branch: $BRANCH)..."
 git clone --depth 1 --recurse-submodules --branch "$BRANCH" "$REPO_URL" "$CLONE_DIR"
 
 echo "- Building site with Hugo v$HUGO_VERSION in $HUGO_DEST..."
-hugo --source "$CLONE_DIR" --destination "$HUGO_DEST" --logLevel info --cleanDestinationDir --gc --panicOnWarning --printI18nWarnings
+hugo --source "$CLONE_DIR" --destination "$HUGO_DEST" --baseURL "$URL" $DRAFTS --logLevel info --cleanDestinationDir --gc --panicOnWarning --printI18nWarnings
 
 echo "- Starting Nginx..."
 exec nginx -g 'daemon off;'
